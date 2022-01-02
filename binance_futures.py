67f90583513f4d25a974577a7771dded7c6454e5
cssmith07060 <cssmith07060@gmail.com>
2022-01-01 19:13:22 -0500

    first commit

--- binance_futures.py
+++ binance_futures.py
@@ -1,6 +1,14 @@
 import logging
-
+import requests
+import pprint
 
 
 logger = logging.getLogger()
 
+
+def get_contracts():
+    response_object = requests.get("https://fapi.binance.com")
+    print(response_object.status_code, response_object.json())
+    pprint.pprint(response_object.json())
+
+    get_contracts()
