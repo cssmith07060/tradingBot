484fff250816b9ce0414e555378d81d02e15e25d
cssmith07060 <cssmith07060@gmail.com>
2022-05-12 06:45:29 -0400

    onyx

--- connectors/interface/styling.py
+++ connectors/interface/styling.py
@@ -1,3 +1,4 @@
 BG_COLLOR = "gray12"
 FG_COLOR = "white"
 FG_COLOR_2 = "SteelBlue1"
+GLOBAL_FONT("Calibri", 11, "normal" )
--- connectors/logging.component.py
+++ connectors/logging.component.py
@@ -6,5 +6,14 @@ class Logging(tk.Frame):
     def __init__(self, *args, **args): 
         super().__init__(*args, **kargs) 
         
-        self.logging_text = tk.Text(self, height=10, width=60, state=tk.DISABLED, bg=BG_COLOR)
+        self.logging_text = tk.Text(self, height=10, width=60, state=tk.DISABLED, bg=BG_COLOR, fg FG_COLOR_2, 
+                                    font= GLOBAL_FONT)
         self.logging_text.pack(side=tx.TOP)
+        
+        
+        def add_log(self, message: str):
+            self.logging_text.configure(state=tk.NORMAL)
+            
+            
+            
+            self.logging_text.configure(state=tk.DISABLED)
