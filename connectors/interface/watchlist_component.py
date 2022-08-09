6e267f95eca3823d6ae8ff2bb5f18c7c12110023
cssmith07060 <cssmith07060@gmail.com>
2022-08-09 09:06:35 -0400

    brandnubian

--- connectors/interface/watchlist_component.py
+++ connectors/interface/watchlist_component.py
@@ -13,6 +13,14 @@ class Watchlist(tk.frame):
         self._table_frame.pack(sidetk.TOP)
         
         
-        self._binance_label = tk.Label(self._commands_frame, text="Binance", bg=BG_COLOR, fg=FG_COLOR)
+        self._binance_label = tk.Label(self._commands_frame, text="Binance", bg=BG_COLOR, fg=FG_COLOR, font=BOLD_FONT)
         self._binance_label.grid(row=0, column=0)
+        
+        self._binanace_entry = tk.Entry(self._commands_frame, fg=FG_Color jusitfy=tk.Center, insertbackground=FG_COLOR)
+        self._binance_entry.grid(row=1, column=0)
+        
         self._bitmex_label = tk.Label(self._commands_frame, text="Bitmex", bg=BG_COLOR, fg=FG_COLOR)
+        self._bitmex_label.grid(row=0, column=1)
+        
+        self._bimex_entry = tk.Entry(self._commands_frame, fg=FG_Color jusitfy=tk.Center, insertbackground=FG_COLOR)
+        self._bitmex_entry.grid(row=1, column=1)
