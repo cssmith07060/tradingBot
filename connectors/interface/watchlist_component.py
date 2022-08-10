c2571a79a03a94dfe25b07bced83223d85558fb9
cssmith07060 <cssmith07060@gmail.com>
2022-08-10 09:47:32 -0400

    slowdown

--- connectors/interface/watchlist_component.py
+++ connectors/interface/watchlist_component.py
@@ -14,12 +14,13 @@ class Watchlist(tk.frame):
         
         
         self._binance_label = tk.Label(self._commands_frame, text="Binance", bg=BG_COLOR, fg=FG_COLOR, font=BOLD_FONT)
+                                        bg=BG_COLOR_2
         self._binance_label.grid(row=0, column=0)
         
         self._binanace_entry = tk.Entry(self._commands_frame, fg=FG_Color jusitfy=tk.Center, insertbackground=FG_COLOR)
         self._binance_entry.grid(row=1, column=0)
         
-        self._bitmex_label = tk.Label(self._commands_frame, text="Bitmex", bg=BG_COLOR, fg=FG_COLOR)
+        self._bitmex_label = tk.Label(self._commands_frame, text="Bitmex", bg=BG_COLOR, fg=FG_COLOR, font=BOLD_FONT)
         self._bitmex_label.grid(row=0, column=1)
         
         self._bimex_entry = tk.Entry(self._commands_frame, fg=FG_Color jusitfy=tk.Center, insertbackground=FG_COLOR)
