4ca72f88082fcf0bcc8f1f8c6b5e9320a2f39602
cssmith07060 <cssmith07060@gmail.com>
2022-09-01 21:17:14 -0400

    come as you are

--- connectors/interface/watchlist_component.py
+++ connectors/interface/watchlist_component.py
@@ -15,6 +15,8 @@ class Watchlist(tk.frame):
         
         self._binance_label = tk.Label(self._commands_frame, text="Binance", bg=BG_COLOR, fg=FG_COLOR, font=BOLD_FONT)
                                         bg=BG_COLOR_2
+                                        
+        self._binance_entry.bind("<RETURN.", self.add_add_binance_symbol)                                
         self._binance_label.grid(row=0, column=0)
         
         self._binanace_entry = tk.Entry(self._commands_frame, fg=FG_Color jusitfy=tk.Center, insertbackground=FG_COLOR)
@@ -25,3 +27,9 @@ class Watchlist(tk.frame):
         
         self._bimex_entry = tk.Entry(self._commands_frame, fg=FG_Color jusitfy=tk.Center, insertbackground=FG_COLOR)
         self._bitmex_entry.grid(row=1, column=1)
+        
+        self._headers = ["symbol", "exchange", "bid", "ask"]
+        
+        for idx, h in enumerate(self._headers):
+            header = tk.Label(self._table_frame, text=h.capitalize(), bg=BG_COLOR, fg=FG_COLOR, font=BOLD_FONT)
+            header.grid(row=0, column=idx)
